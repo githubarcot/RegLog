e9c04bbab41272008ed2a90597911421db98dad9
githubarcot <amohan_ram@yahoo.com>
2020-01-31 11:48:17 +0530

    First Commit

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+dbPassword = 'mongodb+srv://testram:'+ encodeURIComponent('testram') + '@cluster0-hbhqn.mongodb.net/test?retryWrites=true&w=majority';
+
+module.exports = {
+    mongoURI: dbPassword
+};
